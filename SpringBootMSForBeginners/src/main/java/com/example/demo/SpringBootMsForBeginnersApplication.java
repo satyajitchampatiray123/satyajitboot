9607181539958b6daea9fd66846bb7ba4731737f
Satyajit <satyajit_cs@yahoo.co.in>
2022-07-16 00:19:26 +0530

    Spring Boot 1st Project

---
+++ SpringBootMSForBeginners/src/main/java/com/example/demo/SpringBootMsForBeginnersApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMsForBeginnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMsForBeginnersApplication.class, args);
+	}
+
+}
